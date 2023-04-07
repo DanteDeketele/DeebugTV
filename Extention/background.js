589f7141a7b3d9292fae0d44db6068f6952afb0f
Dante Deketele <mrdeegames@gmail.com>
2023-04-07 17:04:52 +0200

    clean update

--- Extention/background.js
+++ Extention/background.js
@@ -1,9 +1,5 @@
 chrome.runtime.onStartup.addListener(function() {
     chrome.tabs.update({ url: "chrome-extension://" + chrome.runtime.id + "/index.html" });
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        { code: "document.body.focus();" }
-      );
   });
 
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
