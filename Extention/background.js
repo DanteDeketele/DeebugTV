c4237a96e45e101dcd66a80ebf3f4cab8fa6357d
Dante Deketele <86111685+MrDeeGames@users.noreply.github.com>
2023-04-07 17:14:10 +0200

    Update background.js

--- Extention/background.js
+++ Extention/background.js
@@ -1,9 +1,1 @@
-chrome.runtime.onStartup.addListener(function() {
-    chrome.tabs.update({ url: "chrome-extension://" + chrome.runtime.id + "/index.html" });
-  });
 
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "openMenuPage") {
-      chrome.tabs.update({ url: chrome.runtime.getURL("index.html") });
-    }
-  });
