a5ea44a975cf1e3ff62415ca1fb12e9383dec9d5
Dante Deketele <mrdeegames@gmail.com>
2023-04-07 16:39:55 +0200

    Update background.js

--- Extention/background.js
+++ Extention/background.js
@@ -1,20 +1,13 @@
 chrome.runtime.onStartup.addListener(function() {
     chrome.tabs.update({ url: "chrome-extension://" + chrome.runtime.id + "/index.html" });
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        { code: "document.body.focus();" }
+      );
   });
 
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "openMenuPage") {
       chrome.tabs.update({ url: chrome.runtime.getURL("index.html") });
     }
-  });
-
-  // Listen for when the extension is first installed or updated
-chrome.runtime.onInstalled.addListener(function() {
-    // Focus on the HTML
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        { code: "document.body.focus();" }
-      );
-    });
   });
