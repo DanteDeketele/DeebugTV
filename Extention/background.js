fd9cbb10e93b70f6461c96b51b29847fbdb27949
Dante Deketele <mrdeegames@gmail.com>
2023-04-09 14:49:02 +0200

    test

--- Extention/background.js
+++ Extention/background.js
@@ -4,12 +4,3 @@
       chrome.tabs.update({ url: chrome.runtime.getURL("index.html") });
     }
   });
-
-  chrome.windows.getCurrent(function (currentWindow) {
-    chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
-      if (activeTabs.length > 0) {
-        chrome.tabs.update(activeTabs[0].id, { active: true });
-      }
-    });
-  });
-  
